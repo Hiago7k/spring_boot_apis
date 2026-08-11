1dd0143687594d3bbbc6ce491854804c4982a744
Hiago 7k <hiago7k@gmail.com>
2026-08-11 17:49:38 -0300

    console

--- src/main/java/br/com/hiago/hiagoAPis/HiagoAPisApplication.java
+++ src/main/java/br/com/hiago/hiagoAPis/HiagoAPisApplication.java
@@ -1,13 +1,18 @@
 package br.com.hiago.hiagoAPis;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HiagoAPisApplication {
+public class HiagoAPisApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HiagoAPisApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Spring boot Console");
+	}
 }
