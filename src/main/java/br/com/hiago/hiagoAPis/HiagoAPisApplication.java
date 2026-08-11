8aa5a264b9c61ea24a0ab1beb64ea6177309db57
Hiago 7k <hiago7k@gmail.com>
2026-08-11 17:52:40 -0300

    contador

--- src/main/java/br/com/hiago/hiagoAPis/HiagoAPisApplication.java
+++ src/main/java/br/com/hiago/hiagoAPis/HiagoAPisApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Scanner;
+
 @SpringBootApplication
 public class HiagoAPisApplication implements CommandLineRunner {
 
@@ -13,6 +15,15 @@ public class HiagoAPisApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
+		Scanner teclado = new Scanner(System.in);
 		System.out.println("Spring boot Console");
+		System.out.println("Digite um numero: ");
+		int numeroDigitado = teclado.nextInt();
+
+		System.out.println("Iniciando contagem até: " + numeroDigitado);
+		for(var i = 1; i <= numeroDigitado; i++){
+			System.out.println(i);
+		}
+
 	}
 }
