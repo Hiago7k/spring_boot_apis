79c4932f542143dbaa561ac8b36449a10e7a24d9
Hiago 7k <hiago7k@gmail.com>
2026-08-11 17:47:28 -0300

    spring boot

---
+++ src/main/java/br/com/hiago/hiagoAPis/HiagoAPisApplication.java
@@ -0,0 +1,13 @@
+package br.com.hiago.hiagoAPis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HiagoAPisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HiagoAPisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/hiago/hiagoAPis/HiagoAPisApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.hiago.hiagoAPis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HiagoAPisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
